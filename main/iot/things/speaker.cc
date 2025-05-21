0cb5759a06cdd32e4faaca0e527e83d64ef9351d
fffqqqhh <sgu_fqh@126.com>
2025-05-21 14:35:55 +0800

    1/修改喇叭音量的调节范围为50-90(原是0-100)

--- main/iot/things/speaker.cc
+++ main/iot/things/speaker.cc
@@ -20,7 +20,7 @@ public:
 
         // 定义设备可以被远程执行的指令
         methods_.AddMethod("SetVolume", "设置音量", ParameterList({
-            Parameter("volume", "0到100之间的整数", kValueTypeNumber, true)
+            Parameter("volume", "50到90之间的整数", kValueTypeNumber, true)
         }), [this](const ParameterList& parameters) {
             auto codec = Board::GetInstance().GetAudioCodec();
             codec->SetOutputVolume(static_cast<uint8_t>(parameters["volume"].number()));
